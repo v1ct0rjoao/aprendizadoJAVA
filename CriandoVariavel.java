f497cd935978533c73207ae845722987fb4b54db
Aluno CAD SENAI BELO JARDIM <aluno.cadbelo@SISTEMAFIEPE.LOCAL>
2024-09-06 13:32:51 -0300

    primeiro commit

---
+++ CriandoVariavel.java
@@ -0,0 +1,17 @@
+public class CriandoVariavel {
+    public static void main(String[] args) {
+        int var1;  //declaração de variavel 
+        int var2;
+
+        // sempre que declarar uma variavel deve seguir o seguinte padrão tipo nomedavariavel
+
+        var1 = 1024; // atribuição de valores de variavel
+
+        System.out.println("Valor da variavel 1: " + var1); // sinal de + diz que o valor vai ser exibido após a string 
+
+        var2 = var1/2;  //existe alguns operadores matemáticos básicos: + - * /
+
+        System.out.println("Varialvel 2 contém o valor de: ");
+        System.out.println(var2);
+    }
+}
---
+++ EntendoDados.java
@@ -0,0 +1,22 @@
+public class EntendoDados {
+    public static void main(String[] args) {
+        
+        int var; //indica valores inteiros
+        double x; // indica varivel com pontos flutuantes
+
+        var = 10;
+        x = 10.0;
+
+        System.out.println("valor de var: " + var);
+        System.out.println("valor de x: " + x);
+        System.out.println();
+
+        var = var/4;
+        x = x/4;
+
+        
+        System.out.println("valor de var agora: " + var);
+        System.out.println("valor de x agora: " + x);
+
+    }
+}
